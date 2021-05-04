d64e643b787ab2f10cb967cd1e8205a598537938
Cooper Hettinger <>
2021-05-04 17:16:59 +0000

    Initial commit

---
+++ main.go
@@ -0,0 +1,11 @@
+package main
+
+import (
+  "fmt"
+  "src/interface/ui.go"
+)
+
+func main() {
+  fmt.Println("Joe Momma")
+  Interface.ui()
+}
---
+++ src/interface/ui.go
@@ -0,0 +1,9 @@
+package ui
+
+import(
+  "fmt"
+)
+
+func Interface(){
+  fmt.Println("Joe Momma but from the UI package")
+}
